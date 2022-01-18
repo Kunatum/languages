d24fe308c585beb5065adbfbf7d90938eeb9a0ad
eRqu <91414875+Kunatum@users.noreply.github.com>
2022-01-18 13:02:41 +0400

    az.js
    Gixfix > all gift web site

---
+++ az.js
@@ -0,0 +1,244 @@
+// Azerbaijan (Azerbaijan)
+// Translated by: eRqu#0271
+
+module.exports.$ = {
+    tr: {
+        iso: "az",
+        name: "Azərbaycanca",
+        country: "aze",
+    }
+};
+
+module.exports = {
+    overview: require("../locales.config.js")["tr"],
+    navbar: {
+        home: 'Ana səyfə',
+        dashboard: 'Yönətim paneli',
+        discover: 'Kəşfet',
+        partners: 'Tərəfdaş',
+        team: 'Qrup',
+        support: 'Dəstək Sunucusu',
+        invite: 'Botu Dəvət Et',
+        login: 'Giriş elə'
+    },
+    footer: {
+        rights: 'Bütün hüquqları gizlidir',
+        description: 'Discorda\'kı ən yaxşı çekiliş botu. Çekilişlər yaradaraq sosyal medya hesaplarınızı doğal olaraq yaxşılaşdırın. sənin əlində...',
+        menus: {
+            first: {
+                title: 'Önəmli',
+                items: {
+                    home: 'Ana Səyfə',
+                    dashboard: 'Yönətim Paneli',
+                    support: 'Dəstək Sunucusu',
+                    add: 'Botu dəvət et'
+                }
+            },
+            second: {
+                title: 'Giftix',
+                items: {
+                    partners: 'Terefdaşlar',
+                    team: 'Qrup'
+                }
+            },
+            third: {
+                title: 'Şirkət',
+                items: {
+                    tos: 'İşlətim qaydaları',
+                    privacy: 'Gizlilik siyasəti'
+                }
+            },
+        },
+        developedWith: 'Swôth & clqu tərəfindən ❤️ ile inkşaf etdirildi.',
+        allServices: 'Bütün xidmətlər işleyir.',
+        someServices: 'Bəzi xidmətler işləmir.'
+    },
+
+
+  //Burayakadar yaptım sonrasını sabah yapcam işim var
+
+
+    index: {
+        title: 'Discord\'daki en iyi çekiliş botu!',
+        description: 'Giftix ile gelişmiş eşantiyonlar oluşturarak sosyal medya hesaplarınızı iyileştirebilir ve organik artış elde edebilirsiniz!',
+        buttons: {
+            dashboard: 'Yönetim Paneli',
+            support: 'Destek Sunucusu',
+            with_discord: 'Discord ile devam et'
+        },
+        box: {
+            title: 'Gelişmiş bir çekiliş oluşturun!',
+            description: 'Giftix ile gelişmiş çekilişler oluşturabilir ve farklı gereksinimler belirleyebilirsiniz! Örneğin, insanların bir çekilişe katılmak için bir sunucuya katılmasını veya kanalınıza abone olmasını sağlayabilirsiniz! Bu, bir çekiliş oluşturmayı daha basit ve kolay hale getirir!',
+            button: {
+                text: 'Şimdi başlayın'
+            }
+        },
+        statistics: {
+            title: 'Sayılarla biz',
+            description: 'Birazda sayısal konuşalım ha ne dersin?',
+            labels: {
+                guilds: 'Sunucu',
+                members: 'Üye',
+                giveaways: 'Çekiliş',
+                joins: 'Katılımcı'
+            }
+        }
+    },
+    discover: {
+        title: 'Keşfet',
+        description: 'Herkese açık çekilişleri burada keşfedin ve ihtiyacınız olan ödülü bulmak ve kazanmak için çekilişlere katılın!',
+        searchInput: 'Ödül, başlık, sunucu adı vb. ile arama yapın.',
+        settings: {
+            title: 'Ayarlar',
+            description: 'Çekiliş, sıralama ayarlarıdır.',
+            amount: {
+                title: 'Miktar',
+                description: 'Listelenecek çekiliş miktarını değiştirmenizi sağlar.'
+            },
+            sort: {
+                title: 'Sıralama',
+                miniNote: 'ödüle göre sıralanır',
+                description: 'Listeleme sırasını değiştirmenizi sağlar.'
+            }
+        },
+        az: '(A-Z) Alfabetik',
+        za: '(Z-A) Alfabetik',
+        noreq: 'Şart gerektirmiyor.',
+        reqs: 'Şartlar'
+    },
+    team: {
+        title: 'Kahramanlarımız',
+        description: 'Giftix\'e çalışan ve katkıda <br> bulunan herkesi bulabilirsiniz. Ekibimize çok teşekkürler. <3',
+        linkcordLang: 'tr',
+        noWidget: 'Araç bulunamadı.',
+
+        roles: {
+            'Founder': 'Kurucu',
+            'Co-Founder': 'Ortak Kurucu',
+            'Community-Manager': 'Topluluk Yöneticisi',
+            'Developer': 'Geliştirici',
+            'Designer': 'Tasarımcı',
+            'Moderator': 'Moderatör'
+        }
+    },
+    create: {
+        title: 'Çekiliş oluştur!',
+        description: 'Bir çekiliş oluşturarak üyelerinize hediyeler dağıtın!',
+        inputs: {
+            title: {
+                title: 'Başlık',
+                description: 'Çekiliş başlığını girin'
+            },
+            description: {
+                title: 'Açıklama',
+                description: 'Çekiliş açıklamasını girin'
+            },
+            prize: {
+                title: 'Ödül',
+                description: 'Çekiliş ödülünü girin'
+            },
+            enddate: {
+                title: 'Bitiş Tarihi',
+                description: 'Çekilişin bitiş tarihi seçin'
+            },
+            winners: {
+                title: 'Kazanan Sayısı',
+                description: 'Çekilişi kazanacak kullanıcı sayısını belirleyin'
+            },
+            channel: {
+                title: 'Kanal',
+                description: 'Çekiliş mesajının gideceği kanalı seçin'
+            },
+            requirements: {
+                title: 'Şartlar',
+                description: 'Çekilişe katılacak üyelerin yapacağı şartları belirleyin'
+            },
+            presentation: {
+                title: 'Görünüm',
+                description: 'Eğer gizliyi seçerseniz çekiliş keşfet kısmında gözükmeyecektir, ama açığı seçerseniz çekiliş keşfette gözükecektir.'
+            },
+        },
+        button: "Oluştur",
+        presentationPrivate: 'Gizli',
+        presentationPublic: 'Açık',
+        requirementsDropdownText: 'Daha fazla seçenek mi arıyorsun? Hesaplarına göz at.',
+        presentationTippy: 'Öneml Ayar',
+        unReachTippy: 'Bu sizin çekiliş oluşturma hakkınızdır. Daha {MORE} çekiliş oluşturabilirsiniz.',
+        reachTippy: 'Yapma ya! Çekiliş oluşturma sınırınıza ulaşmışsınız.',
+        reachedText: 'Çekiliş oluşturma sınırına ulaştınız.<br /> <span class="text-amber-400/50 ml-1 hover:underline cursor-pointer"><i class="fad fa-diamond"></i> Takviye</span> satın alarak çekiliş limitinizi arttırabilirsiniz.'
+    },
+    user: {
+        profile: {
+            giveaways: {
+                title: '{USER}\'nun katıldığı çekilişler'
+            }
+        },
+        dropdown: {
+            profile: 'Profil',
+            guilds: 'Sunucularım',
+            connections: 'Bağlantılar',
+            panel: 'Panel',
+            redeem: 'Kod Bozdur',
+            logout: 'Çıkış yap',
+            _profile: 'Profilini görüntüle!',
+            _guilds: 'Sunucunu seç!',
+            _connections: 'Bağlantıları yönet!',
+            _panel: 'Çok, çok gizli :)',
+            _redeem: 'Kod bozdur!',
+            _logout: 'Website oturumunu sonlandır!'
+        },
+        connections: {
+            title: 'Bağlantılar',
+            description: 'Giftix\'e bağlamış olduğunuz sosyal medya hesaplarını buradan yönetebilirsiniz.',
+            button: {
+                logout: 'Çıkış yapmak için tıkla',
+                connect: '<b>{PROVIDER}</b> ile bağlan',
+                connectMini: 'Bağlamak için tıkla',
+                logged: '<b>{ACCOUNT}</b> olarak bağlandınız'
+            }
+        },
+        redeem: {
+            title: 'Kod Bozdur',
+            description: 'Kodunuzu burada bozdurabilirsiniz.',
+            inputPlaceholder: 'Hediye kodu buraya',
+            button: 'Kod Bozdur',
+            didntWork: 'Kodunuz çalışmadı mı?',
+            success: '🎉 Harika! {COUNT} takviyeyi hesabına ekledik, güle güle bas :)'
+        }
+    },
+    dashboard: {
+        selectServer: {
+            title: 'Sunucu seç',
+            description: 'Sunucunu bulamadın mı? <u>Tekrar giriş</u> yapmayı dene!',
+            serverUnderText: 'Tıkla ve seç.',
+            buttons: {
+                manage: 'Yönet',
+                add: 'Kurulum'
+            }
+        },
+        stats: {
+            total: 'Toplam Çekilişler',
+            active: 'Aktif Çekilişler',
+            activeTippy: 'Limiti artırmak için takviye yapmalısınız!',
+            ended: 'Bitmiş Çekilişler',
+            participants: 'Toplam Katılımcı'
+        },
+        auditLog: {
+            title: 'Denetim Kaydı',
+            description: 'Bu sunucuda Giftix\'te üzerinde yapılan değişikliklerin denetim kaydı burada görülebilir.',
+            table: {
+                user: 'Kullanıcı',
+                date: 'Tarih',
+                action: 'İşlem',
+                message: 'Mesaj'
+            },
+            boost: 'Sunucuya takviye yaptı.',
+            unboost: 'Sunucudaki takviyesini geri çekti.',
+            giveaway_delete: 'Bir çekilişi sildi.',
+            giveaway_create: 'Bir çekiliş başlattı.',
+            giveaway_finish: 'Bir çekiliş sona erdi.',
+            giveaway_join: 'Çekilişe katıldı.',
+            giveaway_reroll: 'Bir çekilişte kazananları yeniledi.'
+        },
+    },
+};
